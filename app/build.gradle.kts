856a675c01f4ad559f34ff7c18337b194d27278d
MaheshSharan <maheshsharan28@gmail.com>
2026-02-26 07:35:17 +0530

    Update build configuration for proper APK signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
 }
 
 dependencies {
